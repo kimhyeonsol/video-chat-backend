a1fe5b27bf4bae509a5a00c4bc4c42160c2d95e5
이윤환 <zoo7930@sju.ac.kr>
2021-08-30 20:00:42 +0900

    Delete SockJS

--- src/main/java/capstone/videochat/WebSocketConfig.java
+++ src/main/java/capstone/videochat/WebSocketConfig.java
@@ -14,7 +14,8 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(signalHandler(), "/signal")
-                .setAllowedOrigins("*").withSockJS(); // allow all origins
+                .setAllowedOrigins("*")
+    // allow all origins
     }
 
     @Bean
