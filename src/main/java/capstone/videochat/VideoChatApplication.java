d1307b35efd1026f2a7ac6d43432dcb77f72c07a
이윤환 <zoo7930@sju.ac.kr>
2021-08-30 19:56:36 +0900

    Add Enable websocket

--- src/main/java/capstone/videochat/VideoChatApplication.java
+++ src/main/java/capstone/videochat/VideoChatApplication.java
@@ -2,8 +2,10 @@ package capstone.videochat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
+@EnableWebSocket
 public class VideoChatApplication {
 
 	public static void main(String[] args) {
